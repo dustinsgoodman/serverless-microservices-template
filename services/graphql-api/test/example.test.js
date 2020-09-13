10b5cae02fef7f892376041f15227e497b81d61d
Dustin Goodman <dustin.s.goodman@gmail.com>
2020-09-13 14:50:20 -0500

    add example test for a service

---
+++ services/graphql-api/test/example.test.js
@@ -0,0 +1,18 @@
+import lambda from '../src/handlers/example';
+
+describe('example', () => {
+  let event;
+
+  beforeEach(() => {
+    event = {
+      body: JSON.stringify({}),
+    };
+  });
+
+  test('returns the response', async () => {
+    const { code, message, statusCode } = await lambda(event, {});
+    expect(code).toBe('OK');
+    expect(message).toEqual(event);
+    expect(statusCode).toBe(200);
+  });
+});
