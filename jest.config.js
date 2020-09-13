722cce825747f9e960d0ac4b3ee2bbe447be2006
Dustin Goodman <dustin.s.goodman@gmail.com>
2020-09-13 14:47:28 -0500

    fix service test pattern

--- jest.config.js
+++ jest.config.js
@@ -158,7 +158,7 @@ module.exports = {
   // The glob patterns Jest uses to detect test files
   testMatch: [
     '<rootDir>/test/**/*.test.js',
-    '<rootDir>/services/**/*.js',
+    '<rootDir>/services/**/*.test.js',
   ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
